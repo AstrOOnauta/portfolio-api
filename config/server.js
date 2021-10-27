6fa0a9301462ff9d3a2da12bdd66f35d87945d8a
AstrOOnauta <willian.ralf.rn@gmail.com>
2021-10-27 06:54:32 -0300

    Started Strapi!

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '4b14a417b4a40fc3ac0f601c4b837751'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'cfac57f8-98f8-4d28-95d8-40e9ca531328'
+};
